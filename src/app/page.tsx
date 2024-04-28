5146379b1d04015182c720348463797873f5dbfe
alexmerino13 <merinoa@oregonstate.edu>
2024-04-28 16:12:54 -0400

    added how to section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import locationImg from '../../public/img/location_icon.png';
+import searchImg from '../../public/img/search_icon.png';
+import treeImg from '../../public/img/tree_icon.png';
 
 export default function Home() {
   return (
+    <div>
     <div className="hero min-h-screen bg-base-200 bg-[url('../../public/img/hero_oranges.jpg')]">
       <div className="hero-content text-center">
         <div className="max-w-xl">
@@ -14,5 +18,46 @@ export default function Home() {
         </div>
       </div>
     </div>
+
+
+      <div className="bg-[#efb61c] p-4">
+        <h2 className="text-center text-2xl font-semibold pt-10 mb-8">How to get started</h2>
+        <div className="flex justify-center pb-10">
+          {/*Card 1*/}
+          <div className="card w-96 bg-base-100 shadow-xl mr-5">
+            <figure className="px-10 pt-10">
+              <Image src={searchImg} alt="Search Icon" className="rounded-xl" />
+            </figure> 
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Step 1</h2>
+              <p>Search for a fruit. Try searching for an apple tree!</p>
+            </div>
+          </div>
+
+          {/*Card 2*/}
+          <div className="card w-96 bg-base-100 shadow-xl mr-5">
+            <figure className="px-10 pt-10">
+              <Image src={locationImg} alt="Map Icon" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Step 2</h2>
+              <p>Allow us to use your location to find nearby fruit trees</p>
+            </div>
+          </div>
+
+          {/*Card 3*/}
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+            <Image src={treeImg} alt="Apple Tree Icon" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Step 3</h2>
+              <p>Pick out a tree to visit!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
   );
 }
