234b74868e32a45d3e3ffc55656701fa5d305b7f
thomatha <thomatha@oregonstate.edu>
2024-05-06 12:11:01 -0700

    add fruit modal, api call, reload map

---
+++ src/components/AddMap.tsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+import Map, { Marker, type MapRef } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import fruitIcon from "@/utils/fruitIcon";
+
+export default function AddMap({ fruit, latitude, longitude, token, onClick }) {
+  const mapRef = useRef<MapRef>();
+  const [isStreet, setIsStreet] = useState(false);
+
+  return (
+    <>
+      <Map
+        ref={mapRef}
+        mapboxAccessToken={token}
+        initialViewState={{
+          longitude: longitude,
+          latitude: latitude,
+          zoom: 19,
+        }}
+        style={{ height: "30vh" }}
+        mapStyle={
+          isStreet
+            ? "mapbox://styles/mapbox/streets-v12"
+            : "mapbox://styles/mapbox/satellite-streets-v12"
+        }
+        onClick={(e) => {
+          onClick(e.lngLat.lat, e.lngLat.lng);
+        }}
+      >
+        <Marker latitude={latitude} longitude={longitude} color="white" />
+        <Marker latitude={latitude} longitude={longitude} offset={[0, -20]}>
+          <span className="text-xl">{fruitIcon(fruit)}</span>
+        </Marker>
+      </Map>
+    </>
+  );
+}
--- src/sql/1.fruits.sql
+++ src/sql/1.fruits.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS Fruits (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
-	color VARCHAR(255) NOT NULL
+	color VARCHAR(255) 
 );
 
 INSERT INTO Fruits (name, color)
 VALUES ('Blackberry', '35, 3, 48'),
-('Golden Plum', '199, 190, 91'),
-('Cherry', '196, 106, 147');
+('Plum', '199, 190, 91'), ('Cherry', '196, 106, 147'), ('Apple', NULL), ('Avocado', NULL), ('Banana', NULL), ('Blueberry', NULL), ('Grape', NULL), ('Green Apple', NULL), ('Honeydew', NULL), ('Kiwi', NULL), ('Lemon', NULL), ('Lime', NULL), ('Mandarin', NULL), ('Mango', NULL), ('Orange', NULL), ('Peach', NULL), ('Pear', NULL), ('Pineapple', NULL), ('Strawberry', NULL), ('Tangerine', NULL), ('Watermelon', NULL);
