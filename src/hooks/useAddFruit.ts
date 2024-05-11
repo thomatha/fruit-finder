a94c910372401394686071722b8d65ec07767bd8
thomatha <thomatha@oregonstate.edu>
2024-05-11 14:06:37 -0700

    add description, camera button

--- src/hooks/useAddFruit.ts
+++ src/hooks/useAddFruit.ts
@@ -1,11 +1,17 @@
 import { Fruit } from "@/types";
 import { useState } from "react";
-const AWS_BUCKET_NAME = 'fruitfinder';
-const AWS_REGION = 'us-east-2';
+const AWS_BUCKET_NAME = "fruitfinder";
+const AWS_REGION = "us-east-2";
 
 // This is the hook return type
 type AddFruitData = [
-  (fruit: Fruit, latitude: number, longitude: number, file: File) => void,
+  (
+    fruit: Fruit,
+    latitude: number,
+    longitude: number,
+    notes: string,
+    file: File
+  ) => void,
   boolean,
   boolean
 ];
@@ -18,50 +24,54 @@ export default function useAddFruit(): AddFruitData {
     fruit: Fruit,
     latitude: number,
     longitude: number,
+    notes: string,
     file: File
   ): Promise<void> {
     setSaving(true);
 
     let s3_img_link = null;
-    if(file) {
-      const response = await fetch('/api/images', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ filename: file.name, contentType: file.type }),
+    if (file) {
+      const response = await fetch("/api/images", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filename: file.name, contentType: file.type }),
       });
-  
+
       if (response.ok) {
         const { url, fields } = await response.json();
-  
+
         const formData = new FormData();
         Object.entries(fields).forEach(([key, value]) => {
-          formData.append(key, value as string)
+          formData.append(key, value as string);
         });
-        formData.append('file', file);
-  
+        formData.append("file", file);
+
         const uploadResponse = await fetch(url, {
-          method: 'POST',
+          method: "POST",
           body: formData,
         });
-  
+
         if (uploadResponse.ok) {
           s3_img_link = `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${fields.key}`;
         } else {
-          console.error('S3 Upload Error:', uploadResponse);
+          console.error("S3 Upload Error:", uploadResponse);
         }
       } else {
-        console.error('Failed to get pre-signed URL.');
+        console.error("Failed to get pre-signed URL.");
       }
     }
+
+    // TODO const s3_img_link = await uploadImage(file);
 
     const data = {
       name: fruit.name,
       fruit_id: fruit.id,
       latitude,
       longitude,
-      s3_img_link
+      description: notes,
+      s3_img_link,
     };
     try {
       const response = await fetch("/api/fruit_locations", {
