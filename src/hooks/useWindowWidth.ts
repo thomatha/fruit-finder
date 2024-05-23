aaa1cd921862967cb3deae09b17e40a3807f55d8
stefan-apr <stefan_apr@hotmail.com>
2024-05-23 10:10:33 -0700

    Update naming conventions

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -1,23 +1,23 @@
 import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
+function getWindowWidth() {
   const { innerWidth: width } = window;
   return {
     width
   };
 }
 
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+export default function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowWidth(getWindowWidth());
     }
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowDimensions;
+  return windowWidth;
 }
